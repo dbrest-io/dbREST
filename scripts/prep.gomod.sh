f4fbd1e4d8e0348e06d87f1738ac9c121d92ff10
Fritz Larco <flarco@live.com>
2024-10-14 11:08:28 -0300

    update prep.gomod.sh

--- scripts/prep.gomod.sh
+++ scripts/prep.gomod.sh
@@ -3,6 +3,7 @@ set -e  # exit on error
 echo 'prep.gomod.sh'
 go mod edit -dropreplace='github.com/flarco/g' go.mod
 go mod edit -dropreplace='github.com/slingdata-io/sling-cli' go.mod
+go mod edit -droprequire='github.com/slingdata-io/sling' go.mod
 # go get github.com/flarco/g@HEAD
 # go get github.com/slingdata-io/sling-cli@HEAD
 go mod tidy
